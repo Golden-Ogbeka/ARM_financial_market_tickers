780847b8627ec545ec9cca160db9e58bb9574eaa
Golden-Ogbeka <57953557+Golden-Ogbeka@users.noreply.github.com>
2022-11-03 15:46:22 +0100

    page bootstrap

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-}
+	reactStrictMode: true,
+	swcMinify: true,
+	env: {},
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
